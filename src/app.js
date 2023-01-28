5b9c13ea4016df11292afc7d6afbe476bc35cb22
SebaVRdev <sebarm177@gmail.com>
2023-01-28 02:47:02 -1100

    Mejorando respuesta entregada en el getPlayers

--- src/app.js
+++ src/app.js
@@ -11,6 +11,15 @@ const app = express();
 //Middleware para que reciba y envie JSON
 app.use(express.json());
 
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); //('*') en vez de eso, cuando lanzemos la pag, se pone el url
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //rutas
 app.use(playersRoutes);
 app.use(seriesRoutes);
